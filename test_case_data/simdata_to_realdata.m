35807c6dba46b7e8fe7235e402e2825d8ab72a5d
Naveen <naveen.13m@gmail.com>
2017-06-12 21:36:01 +0530

    Change the inertial frame to sensor initial frame

--- test_case_data/simdata_to_realdata.m
+++ test_case_data/simdata_to_realdata.m
@@ -1,4 +1,4 @@
-function simdata_to_realdata(robot_make, base_sensor_position_base_frame)
+function simdata_to_realdata(robot_make, base_sensor_position_com_frame)
     % Load simulation data
     curr_dir = pwd;
     data_filename = {'/statevar.dat', '/timevar.dat', '/mtvar.dat'};
@@ -8,17 +8,17 @@ function simdata_to_realdata(robot_make, base_sensor_position_base_frame)
             data_filename{curr_instant}));
     end
     
-    % Compute base sensor frame data based on the base simulation frame data
+    % Compute base sensor data in the modified inertial frame
     len_time = length(timevar);
     cd(strcat(curr_dir, robot_make, '/config_files'));
-    num_links = inputs();
-    num_links = num_links - 1; % Excluding the base link
+    num_links_with_base = inputs();
+    num_links_without_base = num_links_with_base - 1;
     cd(curr_dir);
     base_com_position = statevar(:, 1 : 3);
     base_com_orientation = statevar(:, 4 : 6);
     %joint_position = statvar(:, 7 : 6 + num_links);
-    base_com_lin_vel = statevar(:, 7 + num_links : 9 + num_links);
-    base_com_ang_vel = statevar(:, 10 + num_links : 12 + num_links);
+    base_com_lin_vel = statevar(:, 7 + num_links_without_base : 9 + num_links_without_base);
+    base_com_ang_vel = statevar(:, 10 + num_links_without_base : 12 + num_links_without_base);
     temp = base_com_ang_vel(:, 1);
     base_com_ang_vel(:, 1) = base_com_ang_vel(:, 2);
     base_com_ang_vel(:, 2) = base_com_ang_vel(:, 3);
@@ -27,17 +27,18 @@ function simdata_to_realdata(robot_make, base_sensor_position_base_frame)
     base_sensor_position = zeros(len_time, 3);
     base_sensor_velocity = zeros(len_time, 3);
     for curr_instant = 1 : len_time
-        phi = base_com_orientation(curr_instant);
+        phi = base_com_orientation(curr_instant, 1);
         rot_mat_base = [cos(phi) -sin(phi) 0
                         sin(phi)  cos(phi) 0
                         0         0        1];
         base_sensor_position(curr_instant, :) = ...
         (base_com_position(curr_instant, 1:3).' + ...
-            rot_mat_base*base_sensor_position_base_frame).';
+            rot_mat_base * base_sensor_position_com_frame).' - ...
+            base_sensor_position_com_frame.';
         base_sensor_velocity(curr_instant, :) = ...
         (base_com_lin_vel(curr_instant, 1:3).' + ...
             cross(base_com_ang_vel(curr_instant, 1 : 3).', ...
-            rot_mat_base * base_sensor_position_base_frame)).';         
+            rot_mat_base * base_sensor_position_com_frame)).';         
     end
     
     % Add noise to kinematic data
@@ -50,11 +51,11 @@ function simdata_to_realdata(robot_make, base_sensor_position_base_frame)
     joint_velocity_std_dev = 1;
     std_dev = [base_position_std_dev*ones(1,3), ...
         base_orientation_std_dev*ones(1,3), ...
-        joint_postion_std_dev*ones(1, num_links), ...
+        joint_postion_std_dev*ones(1, num_links_without_base), ...
         base_lin_vel_std_dev*ones(1,3), ...
         base_ang_vel_std_dev*ones(1,3), ...
-        joint_velocity_std_dev*ones(1, num_links)];
-    num_states = 12 + 2 * num_links;
+        joint_velocity_std_dev*ones(1, num_links_without_base)];
+    num_states = 12 + 2 * num_links_without_base;
     noise(len_time, num_states) = 0;
     for curr_state = 1 : num_states
         noise(:, curr_state) = ...
