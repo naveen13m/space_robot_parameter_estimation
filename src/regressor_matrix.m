5f9d038feefc6cef11cfe469de38406493de8df0
Naveen <naveen.13m@gmail.com>
2017-06-29 18:14:44 +0530

    Fix pos. vec. naming and premultipled rot. mat.
    - Consitent and intuitive variable naming of position vectors
    - Premultiplied rotation matrix for base

--- src/regressor_matrix.m
+++ src/regressor_matrix.m
@@ -1,5 +1,6 @@
 function [reg_mat] = regressor_matrix(robot_make,...
-                     base_sensor_position_base_frame, statevar, mtvar)
+                     base_sensor_base_frame_position_base_frame, ...
+                     statevar, mtvar)
     
     % Load robot structural and dynamic parameter data
     curr_dir = pwd;
@@ -12,18 +13,20 @@ function [reg_mat] = regressor_matrix(robot_make,...
     
     % Data assignment
     arm_initial_link_index = find(parent_link_index == 1);
-    num_arms = 0;
-    for curr_initial_link_index = arm_initial_link_index
-        num_arms = num_arms + 1;
-        base_arm_joint_position_base_frame = ...
-            [link_length_DH(curr_initial_link_index);
-            -joint_offset(curr_initial_link_index) * ...
-            sin(joint_twist_angle(curr_initial_link_index)); ...
-             joint_offset(curr_initial_link_index) * ...
-             cos(joint_twist_angle(curr_initial_link_index))];
-        base_arm_joint_position_sensor_frame_sensor_frame(:, num_arms) = ...
-            base_arm_joint_position_base_frame ...
-            - base_sensor_position_base_frame;
+    num_arms = length(arm_initial_link_index);
+    base_arm_joint_sensor_frame_position_sensor_frame = zeros(3, num_arms);
+    curr_arm_index = 0;
+    for curr_arm_initial_link_index = arm_initial_link_index
+        curr_arm_index = curr_arm_index + 1;
+        base_arm_joint_base_frame_position_base_frame = ...
+            [link_length_DH(curr_arm_initial_link_index);
+            -joint_offset(curr_arm_initial_link_index) * ...
+            sin(joint_twist_angle(curr_arm_initial_link_index)); ...
+             joint_offset(curr_arm_initial_link_index) * ...
+             cos(joint_twist_angle(curr_arm_initial_link_index))];
+        base_arm_joint_sensor_frame_position_sensor_frame(:,  curr_arm_index) = ...
+            base_arm_joint_base_frame_position_base_frame ...
+            - base_sensor_base_frame_position_base_frame;
     end
     num_links_without_base = num_links - 1;
     base_sensor_position = statevar(:, 1 : 3).';
@@ -32,33 +35,26 @@ function [reg_mat] = regressor_matrix(robot_make,...
     base_sensor_lin_velocity = statevar(:, 7 + num_links_without_base :...
         9 + num_links_without_base).'; 
     base_sensor_ang_velocity = statevar(:, 10 + num_links_without_base :...
-        12 + num_links_without_base); % ZXY order
-    temp = base_sensor_ang_velocity(:, 1);
-    base_sensor_ang_velocity(:, 1) = base_sensor_ang_velocity(:, 2);
-    base_sensor_ang_velocity(:, 2) = base_sensor_ang_velocity(:, 3);
-    base_sensor_ang_velocity(:, 3) = temp;
-    base_sensor_ang_velocity = base_sensor_ang_velocity.';
+        12 + num_links_without_base).'; 
     joint_ang_speed = statevar(:, 13 + num_links_without_base :...
         12 + 2 * num_links_without_base).';
     num_instants = length(statevar);
-%     joint_ang_position_prev_joint_frame = [base_sensor_orientation(:,1), ...
-%         joint_ang_position];
-%     joint_ang_velocity_prev_joint_frame = [base_sensor_ang_velocity(:, 1), ...
-%         joint_ang_speed];
     
     % Compute rotation matrix of link frames
-    rot_mat_link_prev_link_frame = ...
-        zeros(3, 3, num_instants, num_links);
+    rot_mat_link_prev_link_frame = zeros(3, 3, num_instants, num_links);
     for curr_instant = 1 : num_instants
-        rot_mat_link_prev_link_frame(:, :, curr_instant, 1) = ...
-            rotz(rad2deg(base_sensor_orientation(1, curr_instant))) * ...
-            rotx(rad2deg(base_sensor_orientation(2, curr_instant))) * ...
-            roty(rad2deg(base_sensor_orientation(3, curr_instant))); % ZXY euler angle
+        curr_link_index = 1;
+        phi = base_sensor_orientation(1, curr_instant);
+        theta = base_sensor_orientation(2, curr_instant);
+        psi = base_sensor_orientation(3, curr_instant);
+        rot_mat_link_prev_link_frame(:, :, curr_instant, curr_link_index) = ...
+            ZXY_euler_to_rot_mat(phi, theta, psi);
         for curr_link_index = 2 : num_links
             curr_joint_index = curr_link_index - 1;
-            joint_angle = joint_ang_position(curr_joint_index, curr_instant);
+            curr_joint_angle = joint_ang_position(curr_joint_index, curr_instant);
+            curr_joint_twist = joint_twist_angle(curr_link_index);
             rot_mat_link_prev_link_frame(:, :, curr_instant, curr_link_index) = ...
-                DH_to_rot_mat(joint_twist_angle(curr_link_index), joint_angle);
+                DH_to_rot_mat(curr_joint_twist, curr_joint_angle);
         end
     end
     
@@ -83,42 +79,42 @@ function [reg_mat] = regressor_matrix(robot_make,...
         end
     end
                 
-    % Compute joint position vector
+    % Compute link frame position vector
     link_frame_position = zeros(3, num_instants, num_links);
     link_frame_position(:, :, 1) = base_sensor_position;
-    base_arm_joint_base_sensor = zeros(3, num_instants, num_arms);
+    base_arm_joint_base_sensor_position = zeros(3, num_instants, num_arms);
     
     for curr_instant = 1 : num_instants
         for curr_arm_index = 1 : num_arms
-            base_arm_joint_base_sensor(:, curr_instant, curr_arm_index) ...
+            base_arm_joint_base_sensor_position(:, curr_instant, curr_arm_index) ...
                 = rot_mat_link(:, :, curr_instant, 1) * ...
-                base_arm_joint_position_sensor_frame_sensor_frame(:, curr_arm_index);
+                base_arm_joint_sensor_frame_position_sensor_frame(:, curr_arm_index);
             for curr_link_index = arm_initial_link_index(curr_arm_index) : ...
                     arm_terminal_link_index(curr_arm_index)
                 if curr_link_index ==  arm_initial_link_index(curr_arm_index)
-                    curr_link_frame_position_prev_link_frame = ...
-                        base_arm_joint_base_sensor(:, curr_instant, curr_arm_index);
+                    curr_link_frame_prev_link_frame_position = ...
+                        base_arm_joint_base_sensor_position(:, curr_instant, curr_arm_index);
                     prev_link_frame_position = link_frame_position(:, curr_instant, 1);
                 else
-                    curr_link_frame_position_prev_link_frame_prev_link_frame = ...
+                    curr_link_frame_prev_link_frame_position_prev_link_frame = ...
                         [link_length_DH(curr_link_index);
                          -joint_offset(curr_link_index) * ...
                           sin(joint_twist_angle(curr_link_index)); ...
                           joint_offset(curr_link_index) * ...
                           cos(joint_twist_angle(curr_link_index))];
-                    curr_link_frame_position_prev_link_frame = ...
+                    curr_link_frame_prev_link_frame_position = ...
                         rot_mat_link(:, :, curr_instant, curr_link_index - 1) * ...
-                        curr_link_frame_position_prev_link_frame_prev_link_frame; 
+                        curr_link_frame_prev_link_frame_position_prev_link_frame; 
                     prev_link_frame_position = ...
                         link_frame_position(:, curr_instant, curr_link_index - 1);
                 end
                 link_frame_position(:, curr_instant, curr_link_index) = ...
-                    prev_link_frame_position + curr_link_frame_position_prev_link_frame;
+                    prev_link_frame_position + curr_link_frame_prev_link_frame_position;
             end
         end
     end
     
-    % Compute joint linear and angular velocity in inertial frame
+    % Compute link frame linear and angular velocity in inertial frame
     link_frame_lin_velocity = zeros(3, num_instants, num_links);
     link_frame_ang_velocity = zeros(3, num_instants, num_links);
     link_frame_lin_velocity(:, :, 1) = base_sensor_lin_velocity;
@@ -128,34 +124,50 @@ function [reg_mat] = regressor_matrix(robot_make,...
         for curr_arm_index = 1 : num_arms
             for curr_link_index = arm_initial_link_index(curr_arm_index) : ...
                     arm_terminal_link_index(curr_arm_index)
-                link_frame_position_base_sensor_frame = ...
+                link_frame_base_sensor_frame_position = ...
                     (link_frame_position(:, curr_instant, curr_link_index) - ...
                     link_frame_position(:, curr_instant, 1));
                 link_frame_lin_velocity(:, curr_instant, curr_link_index) = ...
                     link_frame_lin_velocity(:, curr_instant, 1) + ...
                     vec_to_mat(base_sensor_ang_velocity(:, curr_instant)) * ...
-                    link_frame_position_base_sensor_frame;
+                    link_frame_base_sensor_frame_position;
                 link_frame_ang_velocity(:, curr_instant, curr_link_index) = ...
                     base_sensor_ang_velocity(:, curr_instant);
                 for iter_link_index = arm_initial_link_index(curr_arm_index) : ...
-                        curr_link_index % check - Imp
+                        curr_link_index
                     iter_joint_index = iter_link_index - 1;
-                    joint_angular_velocity = ...
-                    rot_mat_link(:, :, curr_link_index) * ...
-                    rot_mat_link_prev_link_frame(:, :, curr_link_index).' * ...
+                    iter_link_angular_velocity = ...
+                    rot_mat_link(:, :, curr_instant, curr_link_index) * ...
                     [0; 0; joint_ang_speed(iter_joint_index, curr_instant)];
-                    link_frame_position_iter_link_frame = ...
+                    link_frame_iter_link_frame_position = ...
                         (link_frame_position(:, curr_instant, curr_link_index) - ...
                         link_frame_position(:, curr_instant, iter_link_index));
                     link_frame_lin_velocity(:, curr_instant, curr_link_index) = ...
                         link_frame_lin_velocity(:, curr_instant, curr_link_index) + ...
-                        vec_to_mat(joint_angular_velocity) * link_frame_position_iter_link_frame;
+                        vec_to_mat(iter_link_angular_velocity) * link_frame_iter_link_frame_position;
                     link_frame_ang_velocity(:, curr_instant, curr_link_index) = ...
                         link_frame_ang_velocity(:, curr_instant, curr_link_index) + ...
-                        joint_angular_velocity;
+                        iter_link_angular_velocity;
                 end
             end
         end
+    end
+    
+    param_vector = zeros(10 * num_links, 1);
+    link_x_com(1) = -base_sensor_base_frame_position_base_frame(1);
+    link_y_com(1) = -base_sensor_base_frame_position_base_frame(2);
+    link_z_com(1) = -base_sensor_base_frame_position_base_frame(3);
+    % param_vector_link = [Iaxx, Iayy, Iazz, Iaxy, Iayz, Iazx, m, max, may, maz].'
+    for curr_link_index = 1 : num_links
+        I_mat_com = [inertia_xx_com(curr_link_index) inertia_xy_com(curr_link_index) inertia_zx_com(curr_link_index); ...
+                     inertia_xy_com(curr_link_index) inertia_yy_com(curr_link_index) inertia_yz_com(curr_link_index); ...
+                     inertia_zx_com(curr_link_index) inertia_yz_com(curr_link_index) inertia_zz_com(curr_link_index)];
+        link_com_link_frame_position = [link_x_com(curr_link_index),...
+            link_y_com(curr_link_index), link_z_com(curr_link_index)].';
+        I_mat_joint = move_inertia_axis(I_mat_com, link_mass(curr_link_index), link_com_link_frame_position);
+        I_vec_joint = inertia_mat_to_vec(I_mat_joint);
+        param_vector(10 * (curr_link_index - 1) + 1 : 10 * (curr_link_index), :) = ...
+            [I_vec_joint; link_mass(curr_link_index); link_mass(curr_link_index) * link_com_link_frame_position];
     end
     
     % Regressor matrix assembly
@@ -184,36 +196,24 @@ function [reg_mat] = regressor_matrix(robot_make,...
     end
      
     % Kinematic data verification    
-    for curr_joint_index = 1 : num_links
+    for curr_link_index = 1 : num_links
         figure(1);
-        joint_pos_x_coord = squeeze(link_frame_position(1, :, curr_joint_index));
-        joint_pos_y_coord  = squeeze(link_frame_position(2, :, curr_joint_index));
-        joint_lin_vel_x_comp = squeeze(link_frame_lin_velocity(1, :, curr_joint_index));
-        joint_lin_vel_y_comp = squeeze(link_frame_lin_velocity(2, :, curr_joint_index));
-        plot(joint_pos_x_coord , joint_pos_y_coord, 'r.', 'LineWidth', 5);
+        link_pos_x_coord = link_frame_position(1, :, curr_link_index);
+        link_pos_y_coord  = link_frame_position(2, :, curr_link_index);
+        link_pos_z_coord  = link_frame_position(3, :, curr_link_index);
+        link_lin_vel_x_comp = link_frame_lin_velocity(1, :, curr_link_index);
+        link_lin_vel_y_comp = link_frame_lin_velocity(2, :, curr_link_index);
+        link_lin_vel_z_comp = link_frame_lin_velocity(3, :, curr_link_index);
+        plot3(link_pos_x_coord , link_pos_y_coord, link_pos_z_coord, ...
+            'r.', 'LineWidth', 5);
         hold on;
-        quiver(joint_pos_x_coord , joint_pos_y_coord, joint_lin_vel_x_comp, ... 
-            joint_lin_vel_y_comp, 'g--');
+        quiver3(link_pos_x_coord , link_pos_y_coord, link_pos_z_coord, ...
+            link_lin_vel_x_comp, link_lin_vel_y_comp, ...
+            link_lin_vel_z_comp, 'g--');
     end
     axis('square'); grid on;
     
     % Momentum verification
-    param_vector = zeros(10 * num_links, 1);
-    link_x_com(1) = -base_sensor_position_base_frame(1);
-    link_y_com(1) = -base_sensor_position_base_frame(2);
-    link_z_com(1) = -base_sensor_position_base_frame(3);
-    % param_vector_link = [Iaxx, Iayy, Iazz, Iaxy, Iayz, Iazx, m, max, may, maz].'
-    for curr_link_index = 1 : num_links
-        I_mat_com = [inertia_xx_com(curr_link_index) inertia_xy_com(curr_link_index) inertia_zx_com(curr_link_index); ...
-                     inertia_xy_com(curr_link_index) inertia_yy_com(curr_link_index) inertia_yz_com(curr_link_index); ...
-                     inertia_zx_com(curr_link_index) inertia_yz_com(curr_link_index) inertia_zz_com(curr_link_index)];
-        link_com_link_frame_position = [link_x_com(curr_link_index),...
-            link_y_com(curr_link_index), link_z_com(curr_link_index)].';
-        I_mat_joint = move_inertia_axis(I_mat_com, link_mass(curr_link_index), link_com_link_frame_position);
-        I_vec_joint = inertia_mat_to_vec(I_mat_joint);
-        param_vector(10 * (curr_link_index - 1) + 1 : 10 * (curr_link_index), :) = ...
-            [I_vec_joint; link_mass(curr_link_index); link_mass(curr_link_index) * link_com_link_frame_position];
-    end
     computed_mtum = reg_mat * param_vector;
         
 %     if ~not_planar
@@ -236,12 +236,19 @@ function [reg_mat] = regressor_matrix(robot_make,...
         legend('Computed momentum', 'Actual momentum', 'Error'); grid on;   
     end
     
+%     save data_planar.mat
     % Delete Px, Py, Lz and zero comps
 %     if ~not_planar
 %         reg_mat(3 : 3 : 3 * num_instants,  :) = [];
 %         reg_mat(:, 4 : 4 : 4 * num_links_with_base) = [];
 %     end
 
+end
+
+function rot_mat = ZXY_euler_to_rot_mat(phi, theta, psi)
+    rot_mat = [cos(phi)*cos(psi)-sin(phi)*sin(theta)*sin(psi)   -sin(phi)*cos(theta)     cos(phi)*sin(psi)+sin(phi)*sin(theta)*cos(psi)
+               sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi)    cos(phi)*cos(theta)     sin(phi)*sin(psi)-cos(phi)*sin(theta)*cos(psi)
+               -cos(theta)*sin(psi)                              sin(theta)              cos(theta)*cos(psi)                         ];
 end
 
 function skew_symm_matrix = vec_to_mat(vector)
@@ -257,7 +264,7 @@ function bullet_notation = bullet(omg)
 end
 
 function I_new = move_inertia_axis(I_old, m, pos_vec)
-    I_new = I_old - m * vec_to_mat(pos_vec) ^ 2;
+    I_new = I_old + m * (pos_vec.' * pos_vec * eye(3) - pos_vec * pos_vec.');
 end
 
 function I_vec = inertia_mat_to_vec(I)
@@ -267,5 +274,5 @@ end
 function rot_mat = DH_to_rot_mat(joint_twist, joint_position)
     rot_mat = [cos(joint_position)                   -sin(joint_position)                     0; ...
                sin(joint_position) * cos(joint_twist) cos(joint_position) * cos(joint_twist) -sin(joint_twist); ... 
-               sin(joint_position) * sin(joint_twist) cos(joint_position) * sin(joint_twist)  cos(joint_twist)]; 
+               sin(joint_position) * sin(joint_twist) cos(joint_position) * sin(joint_twist)  cos(joint_twist)];
 end
