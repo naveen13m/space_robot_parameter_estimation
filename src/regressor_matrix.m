80ef58e96cc13efa22eeec3623da86ae9e59e459
Naveen <naveen.13m@gmail.com>
2017-07-02 18:20:36 +0530

    Plot sys CoM pos append sys CoM equ to reg mat

--- src/regressor_matrix.m
+++ src/regressor_matrix.m
@@ -40,9 +40,18 @@ function [reg_mat] = regressor_matrix(robot_make,...
     joint_ang_speed = statevar(:, 13 + num_links_without_base :...
         12 + 2 * num_links_without_base).';
     num_instants = size(statevar, 1);
+    link_x_com(1) = -base_sensor_base_frame_position_base_frame(1);
+    link_y_com(1) = -base_sensor_base_frame_position_base_frame(2);
+    link_z_com(1) = -base_sensor_base_frame_position_base_frame(3);
+    
+    % Regressor matrix data initialization
+    rot_mat_link_prev_link_frame = zeros(3, 3, num_instants, num_links);
+    rot_mat_link = zeros(3, 3, num_instants, num_links);
+    link_frame_position = zeros(3, num_instants, num_links);
+    link_frame_lin_velocity = zeros(3, num_instants, num_links);
+    link_frame_ang_velocity = zeros(3, num_instants, num_links);
     
     % Compute orientation of link frames - Rotation matrix
-    rot_mat_link_prev_link_frame = zeros(3, 3, num_instants, num_links);
     for curr_instant = 1 : num_instants
         curr_link_index = 1;
         phi = base_sensor_orientation(1, curr_instant);
@@ -50,19 +59,14 @@ function [reg_mat] = regressor_matrix(robot_make,...
         psi = base_sensor_orientation(3, curr_instant);
         rot_mat_link_prev_link_frame(:, :, curr_instant, curr_link_index) = ...
             ZXY_euler_to_rot_mat(phi, theta, psi);
+        rot_mat_link(:, :, curr_instant, 1) = ...
+            rot_mat_link_prev_link_frame(:, :, curr_instant, 1);
         for curr_link_index = 2 : num_links
             curr_joint_index = curr_link_index - 1;
             curr_joint_angle = joint_ang_position(curr_joint_index, curr_instant);
             curr_joint_twist = joint_twist_angle(curr_link_index);
             rot_mat_link_prev_link_frame(:, :, curr_instant, curr_link_index) = ...
                 DH_to_rot_mat(curr_joint_twist, curr_joint_angle);
-        end
-    end
-    
-    rot_mat_link = zeros(3, 3, num_instants, num_links);
-    rot_mat_link(:, :, :, 1) = rot_mat_link_prev_link_frame(:, :, :, 1);
-    for curr_instant = 1 : num_instants
-        for curr_link_index = 2 : num_links
             pli = parent_link_index(curr_link_index);
             rot_mat_prev_link = rot_mat_link(:, :, curr_instant, pli);
             rot_mat_link(:, :, curr_instant, curr_link_index) = ...
@@ -70,12 +74,10 @@ function [reg_mat] = regressor_matrix(robot_make,...
                 rot_mat_link_prev_link_frame(:, :, curr_instant, curr_link_index);
         end
     end
-                
+    
     % Compute link frame position vector
-    link_frame_position = zeros(3, num_instants, num_links);
     link_frame_position(:, :, 1) = base_sensor_position;
     base_arm_joint_base_sensor_position = zeros(3, num_instants, num_arms);
-    
     for curr_instant = 1 : num_instants
         for curr_arm_index = 1 : num_arms
             base_arm_joint_base_sensor_position(:, curr_instant, curr_arm_index) ...
@@ -107,11 +109,8 @@ function [reg_mat] = regressor_matrix(robot_make,...
     end
     
     % Compute link frame linear and angular velocity in inertial frame
-    link_frame_lin_velocity = zeros(3, num_instants, num_links);
-    link_frame_ang_velocity = zeros(3, num_instants, num_links);
     link_frame_lin_velocity(:, :, 1) = base_sensor_lin_velocity;
     link_frame_ang_velocity(:, :, 1) = base_sensor_ang_velocity;
-    
     for curr_instant = 1 : num_instants
         for curr_arm_index = 1 : num_arms
             for curr_link_index = arm_initial_link_index(curr_arm_index) : ...
@@ -142,15 +141,55 @@ function [reg_mat] = regressor_matrix(robot_make,...
         end
     end
     
+    % Regressor matrix assembly
+    num_equs = 9;
+    num_link_params = 10;
+    reg_mat = zeros(num_equs * num_instants, num_link_params * num_links);
+    for curr_instant = 1 : num_instants
+        for curr_link_index = 1 : num_links
+            submatrix_11 = zeros(3, 6);
+            submatrix_12 = link_frame_lin_velocity(:, curr_instant, curr_link_index);
+            ang_vel_matrix = vec_to_mat(link_frame_ang_velocity(:, curr_instant, curr_link_index));
+            submatrix_13 = ang_vel_matrix * rot_mat_link(:, :, curr_instant, curr_link_index);
+            submatrix_21 = rot_mat_link(:, :, curr_instant, curr_link_index) * ...
+                bullet(rot_mat_link(:, :, curr_instant, curr_link_index).' * ...
+                link_frame_ang_velocity(:, curr_instant, curr_link_index));
+            submatrix_22 = cross(link_frame_position(:, curr_instant, curr_link_index), ...
+                submatrix_12);
+            submatrix_23 = (vec_to_mat(link_frame_position(:, curr_instant, curr_link_index)) * ...
+                vec_to_mat(link_frame_ang_velocity(:, curr_instant, curr_link_index)) - ...
+                vec_to_mat(submatrix_12)) * rot_mat_link(:, :, curr_instant, curr_link_index);
+            submatrix_31 = submatrix_11;
+            submatrix_32 = link_frame_position(:, curr_instant, curr_link_index);
+            submatrix_33 = rot_mat_link(:, :, curr_instant, curr_link_index);
+            link_mat = [submatrix_11, submatrix_12, submatrix_13; ...
+                        submatrix_21, submatrix_22, submatrix_23; ...
+                        submatrix_31, submatrix_32, submatrix_33];
+            curr_instant_reg_mat(:, num_link_params * (curr_link_index - 1) + 1 :...
+                num_link_params * curr_link_index) = link_mat;
+        end
+        reg_mat(num_equs * (curr_instant - 1) + 1 : num_equs * curr_instant, :) = curr_instant_reg_mat;
+    end
+     
+    % Kinematic data and momentum verification    
+    plot_kinematic_data(num_links, link_frame_position, link_frame_lin_velocity,...
+        link_mass, link_x_com, link_y_com, link_z_com, rot_mat_link, num_instants);
+    param_vector = construct_dyn_param_vector(...
+        inertia_xx_com, inertia_yy_com, inertia_zz_com, inertia_xy_com, inertia_yz_com,...
+        inertia_zx_com, link_mass, link_x_com, link_y_com, link_z_com, num_links);
+    computed_mtum = reg_mat * param_vector;
+    plot_momentum_data(num_instants, num_equs, computed_mtum, mtvar);
+    
+end
+
+function param_vector = construct_dyn_param_vector(...
+        inertia_xx_com, inertia_yy_com, inertia_zz_com, inertia_xy_com, inertia_yz_com,...
+        inertia_zx_com, link_mass, link_x_com, link_y_com, link_z_com, num_links)
     param_vector = zeros(10 * num_links, 1);
-    link_x_com(1) = -base_sensor_base_frame_position_base_frame(1);
-    link_y_com(1) = -base_sensor_base_frame_position_base_frame(2);
-    link_z_com(1) = -base_sensor_base_frame_position_base_frame(3);
     I_mat_com_com = zeros(3, 3, num_links);
     I_mat_joint_joint = zeros(3, 3, num_links);
     I_vec_joint_joint = zeros(6, num_links);
     link_com_link_frame_position_link_frame = zeros(3, num_links);
-    % param_vector_link = [Iaxx, Iayy, Iazz, Iaxy, Iayz, Iazx, m, max, may, maz].'
     for curr_link_index = 1 : num_links
         I_mat_com_com(:, :, curr_link_index) = ...
             [inertia_xx_com(curr_link_index) inertia_xy_com(curr_link_index) inertia_zx_com(curr_link_index); ...
@@ -169,35 +208,16 @@ function [reg_mat] = regressor_matrix(robot_make,...
              link_mass(curr_link_index); ...
              link_mass(curr_link_index) * link_com_link_frame_position_link_frame(:, curr_link_index)];
     end
-    
-    % Regressor matrix assembly
-    reg_mat = zeros(6 * num_instants, 10 * num_links);
-    for curr_instant = 1 : num_instants
-        curr_instant_reg_mat = zeros(6, 10 * num_links);
-        for curr_link_index = 1 : num_links
-            submatrix_11 = zeros(3, 6);
-            submatrix_12 = link_frame_lin_velocity(:, curr_instant, curr_link_index);
-            ang_vel_matrix = vec_to_mat(link_frame_ang_velocity(:, curr_instant, curr_link_index));
-            submatrix_13 = ang_vel_matrix * rot_mat_link(:, :, curr_instant, curr_link_index);
-            submatrix_21 = rot_mat_link(:, :, curr_instant, curr_link_index) * ...
-                bullet(rot_mat_link(:, :, curr_instant, curr_link_index).' * ...
-                link_frame_ang_velocity(:, curr_instant, curr_link_index));
-            submatrix_22 = cross(link_frame_position(:, curr_instant, curr_link_index), ...
-                submatrix_12);
-            submatrix_23 = (vec_to_mat(link_frame_position(:, curr_instant, curr_link_index)) * ...
-                vec_to_mat(link_frame_ang_velocity(:, curr_instant, curr_link_index)) - ...
-                vec_to_mat(submatrix_12)) * rot_mat_link(:, :, curr_instant, curr_link_index);
-            link_mat = [submatrix_11, submatrix_12, submatrix_13; ...
-                        submatrix_21, submatrix_22, submatrix_23];
-            curr_instant_reg_mat(:, 10 * curr_link_index - 9 : 10 * curr_link_index) = ...
-                link_mat;
-        end
-        reg_mat(6 * curr_instant - 5 : 6 * curr_instant, :) = curr_instant_reg_mat;
-    end
-     
-    % Kinematic data verification    
+end
+
+function plot_kinematic_data(num_links, link_frame_position, link_frame_lin_velocity,...
+        link_mass, link_x_com, link_y_com, link_z_com, rot_mat_link, num_instants)
+    figure();
+    sys_pos_x_coord = zeros(1, size(link_frame_position, 2));
+    sys_pos_y_coord = zeros(1, size(link_frame_position, 2));
+    sys_pos_z_coord = zeros(1, size(link_frame_position, 2));
+    link_com_link_frame_position = zeros(3, num_instants, num_links);
     for curr_link_index = 1 : num_links
-        figure(1);
         link_pos_x_coord = link_frame_position(1, :, curr_link_index);
         link_pos_y_coord  = link_frame_position(2, :, curr_link_index);
         link_pos_z_coord  = link_frame_position(3, :, curr_link_index);
@@ -210,23 +230,51 @@ function [reg_mat] = regressor_matrix(robot_make,...
         quiver3(link_pos_x_coord , link_pos_y_coord, link_pos_z_coord, ...
             link_lin_vel_x_comp, link_lin_vel_y_comp, ...
             link_lin_vel_z_comp, 'g--');
+        link_com_link_frame_position_link_frame = ...
+                                        [link_x_com(curr_link_index);...
+                                        link_y_com(curr_link_index);...
+                                        link_z_com(curr_link_index)];
+        for curr_instant = 1 : num_instants
+            link_com_link_frame_position(:, curr_instant, curr_link_index) = ...
+                rot_mat_link(:, :, curr_instant, curr_link_index) * ...
+                link_com_link_frame_position_link_frame;
+        end
+        sys_pos_x_coord = sys_pos_x_coord + ...
+            link_mass(curr_link_index) * (link_pos_x_coord + ...
+            link_com_link_frame_position(1, :, curr_link_index));
+        sys_pos_y_coord = sys_pos_y_coord + ...
+            link_mass(curr_link_index) * (link_pos_y_coord + ...
+            link_com_link_frame_position(2, :, curr_link_index));
+        sys_pos_z_coord = sys_pos_z_coord + ...
+            link_mass(curr_link_index) * (link_pos_z_coord + ...
+            link_com_link_frame_position(3, :, curr_link_index));
     end
-    axis('square'); grid on;
-    
-    % Momentum verification
-    computed_mtum = reg_mat * param_vector;
-              
+    sys_mass = sum(link_mass);
+    sys_pos_x_coord = sys_pos_x_coord / sys_mass;
+    sys_pos_y_coord = sys_pos_y_coord / sys_mass;
+    sys_pos_z_coord = sys_pos_z_coord / sys_mass;
+    plot3(sys_pos_x_coord , sys_pos_y_coord, sys_pos_z_coord, ...
+            'k.', 'LineWidth', 2);
+    set(gcf,'units','pixels','position',[0, 0, 1366, 768]);
+    axis('square'); grid on; drawnow;
+end
+
+function plot_momentum_data(num_instants, num_equs, computed_mtum, mtvar)
+    figure();
     for direction_index = 1 : 6
         directional_mtum_computed = ...
-            computed_mtum(direction_index : 6 : 6 * num_instants);
+            computed_mtum(direction_index : num_equs : num_equs * num_instants);
         directional_mtum_actual = mtvar(:, direction_index);
         directional_mtum_error =  directional_mtum_actual - ...
             directional_mtum_computed;
-        figure();
+        subplot(2, 3, direction_index);
         plot(directional_mtum_computed); hold on;
         plot(directional_mtum_actual);
         plot(directional_mtum_error, 'r');
-        legend('Computed momentum', 'Actual momentum', 'Error'); grid on;   
+        grid on;
     end
+    hL = legend('Computed momentum', 'Actual momentum', 'Error');
+    set(hL,'Position', [0.50 0.48 0.05 0.05], 'Units', 'normalized');
+    set(gcf,'units','pixels','position',[0, 0, 1366, 768]);
+    drawnow;
 end
-        
