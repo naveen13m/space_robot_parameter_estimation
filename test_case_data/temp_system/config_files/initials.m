0aeada7f5fafc7ebcfc92a1b344bb2b2c5d05193
Naveen <naveen.13m@gmail.com>
2017-07-22 21:53:33 +0530

    Compute momentum about sensor-based ref frame

---
+++ test_case_data/temp_system/config_files/initials.m
@@ -0,0 +1,26 @@
+% ReDySim initials module. The initial conditions are entered in this module
+% Contibutors: Dr. Suril Shah and Prof S. K. Saha @IIT Delhi
+
+function [y0, ti, tf, incr, rtol, atol]=initials()
+
+%Simulation time
+ti=0;
+tf=3;
+
+%Inverse kinematics for obtaining initial configuration
+[n]=inputs();
+
+%Base motions
+q=[0; 0; 0; 0; 0; 0];
+dq=[0; 0; 0; 0; 0; 0];
+
+% Actuator energy
+acten=0;
+
+%Vecotor of all the initial State Variable
+y0=[q; dq; acten];
+
+%INTERATION TOLERANCES
+incr=0.001;
+rtol=1e-5;         %relative tolerance in integration 
+atol=1e-7;         %absolute tolerances in integration 
---
+++ test_case_data/temp_system/config_files/inputs.m
@@ -0,0 +1,44 @@
+% ReDySim inputs module. The model parameters are entered in this module
+% Contibutors: Dr. Suril Shah and Prof S. K. Saha @IIT Delhi
+
+function [n nq alp a b bt dx dy dz al alt m g  Icxx Icyy Iczz Icxy Icyz Iczx]=inputs()
+
+% 3 Link Manipulator
+%NO. OF LINKS
+n=2;
+
+nq=0;%1 for spatial and 0 for planar
+%ENTER DH PARAMETER HERE   
+%  dh=[al b alp th];
+alp=[0; 0];
+a=[0; 0.5];
+b=[0; 0];
+%Parent array bt and corrosponding vectors
+bt=[0 1];
+
+%Link Length
+al=[1; 1];
+% %Distance from origin to link tip in term of link length
+alt=[0.5; 1];
+
+%ENTER VECTOR dm
+dx=[  0    al(2)/2 ];
+dy=[  0       0    ];
+dz=[  0       1  ];
+
+
+%MASS
+m=[1000; 100];
+% g=[0 ; -9.81; 0];
+ g=[0 ; 0; 0];
+
+%MOMENT OF INERTIA
+Icxx=zeros(n,1);Icyy=zeros(n,1);Iczz=zeros(n,1); % Initialization 
+Icxy=zeros(n,1);Icyz=zeros(n,1);Iczx=zeros(n,1); % Initialization 
+% Icxx(1)=(1/12)*0.01*0.01;   Icyy(1)=(1/12)*m(1)*(al(1)*al(1)+0.1*0.1);  Iczz(1)=(1/12)*m(1)*al(1)*al(1);
+% Icxx(2)=(1/12)*0.01*0.01;   Icyy(2)=(1/12)*m(2)*al(2)*al(2);  Iczz(2)=(1/12)*m(2)*al(2)*al(2);
+% Icxx(3)=(1/12)*0.01*0.01;   Icyy(3)=(1/12)*m(3)*al(3)*al(3);  Iczz(3)=(1/12)*m(3)*al(3)*al(3);
+% Icxx(4)=(1/12)*0.01*0.01;   Icyy(4)=(1/12)*m(4)*al(4)*al(4);  Iczz(4)=(1/12)*m(4)*al(4)*al(4);
+
+Icxx(1)=1200;  Icyy(1)=1200; Iczz(1)=1200;
+Icxx(2)=120;   Icyy(2)=120;  Iczz(2)=120;
