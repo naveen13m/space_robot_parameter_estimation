1b754ce464b7abcfed707452979220df6947b8cf
Naveen <naveen.13m@gmail.com>
2017-08-22 17:12:04 +0530

    Obtain solution for coupled params.

--- src/run_me.m
+++ src/run_me.m
@@ -2,18 +2,20 @@ clear all;
 close all;
 clc;
 
-% dbstop in regressor_matrix.m  at 213
+% global to_check
+% dbstop in global_kinematic_matrix.m  at 196
 % Configuration parameters
-robot_make = '/1_link_rw';
+% robot_make = '/1_link_rw';
 % robot_make = '/1_link_2rw';
 % robot_make = '/2_link';
-% robot_make = '/3_link';
+% robot_make = '/2_link_mrw';
+robot_make = '/3_link';
 % robot_make = '/4_link';
 % robot_make = '/8_link';
 % robot_make = '/dual_arm';
 % robot_make = '/2_link_rw';
 % robot_make = '/4_link_spatial';
-% robot_make = '/temp_system';
+% robot_make = '/temp';
 % robot_make = '/static_base';
 
 base_sensor_position_base_frame = [0; 0; 0];
@@ -44,67 +46,56 @@ cd(curr_dir);
 num_instants = size(statevar, 1);
 
 % Populate the linear momentum regressor matrix
-global_kin_mat_1 = global_kinematic_matrix(robot_make, ...
+global_kin_mat = global_kinematic_matrix(robot_make, ...
     base_sensor_position_base_frame, statevar, mtvar);
-reg_mat = remove_excess(global_kin_mat_1);
-rref_reg_mat = rref(reg_mat, 10^-5);
-rref_global_kin_mat = rref(global_kin_mat_1);
+reg_mat = remove_excess(global_kin_mat);
+rref_reg_mat = rref(reg_mat, 10e-5);
+[rref_global_kin_mat, base_col_index] = rref(global_kin_mat);
 
-% b_actual = -10 * reg_mat(:, 5) - 50 * reg_mat(:, 6);
-% A = reg_mat(:, 1 : 3);
-% x_comp = pinv(A) * b_actual;
-% % x_actual = [1330 - 0.466969034720210 * 300; 1000 - 3.333334097042634 * 300; 200 - 0.746032174302360 * 300]; % 0.5
-% x_actual = [1330 - 0.504384007488360 * 300; 1000 - 3.333334108112251 * 300; 200 - 0.825397163698678 * 300]; % 1 
-% % x_actual = [1330 - 0.500826351786586 * 300; 1000 - 3.333335493225453 * 300; 200 - 0.818182372089360 * 300]; % 0.5, diff mtum wheel dyna params
-% % x_actual = [1330 - 0.567592476990328 * 300; 1000 - 3.333333895966867 * 300; 200 - 0.944444794646752 * 300]; %0.5 different base params
-% x_actual_comp_error = [x_actual, x_comp, x_actual - x_comp]
-% 
-% b_comp = A * x_actual;
-% rhs_actual_comp_error = [b_actual, b_comp, b_actual - b_comp];
-% 
-% mtum_actual = A * x_actual - b_actual;
-% mtum_comp = A * x_comp - b_actual;
-% 
-% figure(3);
-% subplot(2, 3, 1);
-% plot(b_actual(1 : 3 : end), 'g'); hold on;
-% plot(b_comp(1 : 3 : end), 'r--')
-% 
-% subplot(2, 3, 2);
-% plot(b_actual(2 : 3 : end), 'g'); hold on;
-% plot(b_comp(2 : 3 : end), 'r--')
-% 
-% subplot(2, 3, 3);
-% plot(b_actual(3 : 3 : end), 'g'); hold on;
-% plot(b_comp(3 : 3 : end), 'r--')
-% 
-% subplot(2, 3, 4);
-% plot(mtum_actual(1 : 3 : end), 'g'); hold on;
-% plot(mtum_comp(1 : 3 : end), 'r--')
-% 
-% subplot(2, 3, 5);
-% plot(mtum_actual(2 : 3 : end), 'g'); hold on;
-% plot(mtum_comp(2 : 3 : end), 'r--')
-% 
-% subplot(2, 3, 6);
-% plot(mtum_actual(3 : 3 : end), 'g'); hold on;
-% plot(mtum_comp(3 : 3 : end), 'r--')
+reg_mat = global_kin_mat(:, [3, 7, 8, 9, 13, 18, 19, 23, 28, 29]);
+reg_mat([1 : 6, 9 : 6 : end, 10 : 6 : end, 11 : 6 : end]) = [];
+reg_mat = global_kin_mat(:, [3, 7, 8, 9, 13, 18, 19, 23, 28, 29]);
+reg_mat([1 : 6, 9 : 6 : end, 10 : 6 : end, 11 : 6 : end], :) = [];
+b = - 1080 * reg_mat(:, 2);
+A = reg_mat(:, [1, 3 : end]);
 
+% % disp('Simple LS')
+% ls_sol = pinv(A) * b
 
-% actual_coeffs = rref_global_kin_mat(1 : 6, 20)
-% l = [0.5; 0; 0] - base_sensor_position_base_frame
-% al = joint_twist_angle(2)
-% computed_coeffs = [2 * l(3) * cos(al) - 2 * l(2) * sin(al);
-%                    2 * l(3) * cos(al); 
-%                    -2 * l(2) * sin(al);
-%                    l(1) * sin(al);
-%                    l(3) * sin(al) - l(2) * cos(al);
-%                    - l(1) * cos(al)]
-                    
-% reg_mat(601, :) = [1, 0, 0, 1, 0, 0];
-% rank(reg_mat)
-% cond(reg_mat)
-% reg_mat(:, 4) = [];
-% null(reg_mat)
-% cond(reg_mat(:, 2:5))
-% sol = -1100 * pinv(reg_mat(:, 2 : 5)) * reg_mat(:, 1)
+%    --    Base   ---  --  Link-1 --  --  Link-2   --
+lb = [400, -100, -100,    0,   0,  0,    15,   0,  0];
+ub = [600,  100,  100,  100, 100,  2,    30,  20,  5];
+act_sol = [520,   40,    0, 62.5,  55,  0,  17.5,  15,  0];
+
+% % Verification
+mtum_act = A*act_sol.' - b;
+figure(1);
+plot(mtum_act(1 : 3 : end)); hold on;
+figure(2);
+plot(mtum_act(2 : 3 : end)); hold on;
+figure(3);
+plot(mtum_act(3 : 3 : end)); hold on;
+
+% % Constrained LS
+disp('Constrained LS')
+[cls_sol, resnorm, ~, exitflag, output, lambda] = lsqlin(A, b, [], [], [], [], lb, ub, ub)
+
+% % Particle filter
+% options = optimoptions('particleswarm','SwarmSize',200,'HybridFcn',@patternsearch);
+% [pf_sol, fval, exitflag, output] = particleswarm(@(x)(norm((A * x.' - b))), 9, lb, ub, options)
+
+% % Global search
+disp('Global search')
+gs = GlobalSearch;
+opts = optimoptions(@fmincon,'Algorithm','sqp');
+problem = createOptimProblem('fmincon','objective',@(x)norm(A * x.' - b),'lb',lb,'ub',ub,'x0',cls_sol.', 'options', opts);
+[gs_sol, res_norm] = run(gs,problem)
+
+% Verification
+mtum_comp = A*gs_sol.' - b;
+figure(1);
+plot(mtum_comp(1 : 3 : end)); legend('P_{xa}','P_{xx}');
+figure(2);
+plot(mtum_comp(2 : 3 : end)); legend('P_{ya}', 'P_{yc}');
+figure(3);
+plot(mtum_comp(3 : 3 : end)); legend('L_{za}', 'L_{zc}');
