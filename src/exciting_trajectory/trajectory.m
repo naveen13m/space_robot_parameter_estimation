9977f3ac6c3f095953a0cda8b1e188adf2159039
Naveen <naveen.13m@gmail.com>
2017-08-28 16:07:34 +0530

    Fourier base joint pos, vel, and acc computation

---
+++ src/exciting_trajectory/trajectory.m
@@ -0,0 +1,8 @@
+function [th_d, dth_d, ddth_d]=trajectory(t, n, tf, tr_par)
+    th_d = zeros(n-1,1); dth_d = zeros(n-1,1); ddth_d = zeros(n-1,1); 
+    for i=1:n-1
+        th_d(i, 1) = fourier_series(tr_par(i,:), t);
+        dth_d(i, 1)= dfourier(tr_par(i,:), t);
+        ddth_d(i, 1)= ddfourier(tr_par(i,:), t);
+    end
+end
