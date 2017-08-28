2198ca2518ca650592bdd93d7a9d76d2c67ce7f9
Naveen <naveen.13m@gmail.com>
2017-08-28 23:23:59 +0530

    Add realistic limits

--- src/exciting_trajectory/joint_limits.m
+++ src/exciting_trajectory/joint_limits.m
@@ -1,10 +1,10 @@
-function [min_jt_angle, max_jt_angle, min_jt_speed, max_jt_speed, ...
+function [min_jt_angle, max_jt_angle, min_jt_velocity, max_jt_velocity, ...
                                    min_jt_acc, max_jt_acc] = joint_limits()
-    min_jt_angle = [-pi/2;  -2 * pi/3];
-    max_jt_angle = [pi/2;  2 * pi/3];
-    min_jt_speed = [-pi/2;  -2 * pi/3];
-    max_jt_speed = [pi/2;  2 * pi/3];
-    min_jt_acc = [-pi/2;  -2 * pi/3];
-    max_jt_acc = [pi/2;  2 * pi/3];
+    min_jt_angle = [-90; -110] * pi/180;
+    max_jt_angle = [90; 110] * pi/180;
+    min_jt_velocity = [-25; -25] * pi/180;
+    max_jt_velocity = [25; 25] * pi/180;
+    min_jt_acc = [-15; -15] * pi/180;
+    max_jt_acc = [15; 15] * pi/180;
 end
 
