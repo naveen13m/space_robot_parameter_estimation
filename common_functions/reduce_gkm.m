84fe29d72811a4efeede743430c1525abb2955c1
Naveen <naveen.13m@gmail.com>
2017-08-30 20:55:49 +0530

    Fix output vec comp. and typo in spatial case

--- common_functions/reduce_gkm.m
+++ common_functions/reduce_gkm.m
@@ -1,11 +1,12 @@
 function [reg_mat, output_vec] = reduce_gkm(gkm, is_planar)
     if is_planar
-        reduced_mat = eliminate_dependency(gkm);
+        reg_mat = eliminate_dependency(gkm);
+        output_vec = -reg_mat(:, 2);
+        reg_mat(:, 2) = [];
     else
-        reduced_mat = gkm;
-        reduced_mat(:, [12 : 10 : end, 17 : 19 : end, 20 : 10 : end]) = [];
+        reg_mat = gkm;
+        output_vec = -reg_mat(:, 7);
+        reg_mat(:, [7, 12 : 10 : end, 17 : 10 : end, 20 : 10 : end]) = [];
     end
-    reg_mat = reduced_mat(:, [1, 3 : end]);
-    output_vec = -reduced_mat(:, 2);
 end
 
