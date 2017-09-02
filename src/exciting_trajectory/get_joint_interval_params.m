e942580d6ca4fc54cf8cde737f8b62914a7f148f
Naveen <naveen.13m@gmail.com>
2017-09-03 00:23:10 +0530

    Compute th, dth & ddth of all jts at given instant
    - Use cycloidal parametrization
    - Compute the data for trajectory with multiple intervals

---
+++ src/exciting_trajectory/get_joint_interval_params.m
@@ -0,0 +1,10 @@
+function [thi, thf, curr_time] = get_joint_interval_params(tr_par, curr_time, Tp, curr_joint, num_joints)
+    num_intervals_each_joint = 2 ^ (num_joints - 1);
+    num_terms_each_joint = num_intervals_each_joint + 1;
+    num_terms_till_curr_joint = num_terms_each_joint * (curr_joint - 1);
+    time_params = repmat(Tp, 1, num_intervals_each_joint);
+    interval_index = get_joint_interval_index(curr_time, time_params);
+    thi = tr_par(num_terms_till_curr_joint + interval_index);
+    thf = tr_par(num_terms_till_curr_joint + interval_index + 1);
+    curr_time = curr_time - (interval_index - 1) * Tp;
+end
