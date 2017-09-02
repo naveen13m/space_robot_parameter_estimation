0a19b2040bfab8c154c5546a16113612583cc621
Naveen <naveen.13m@gmail.com>
2017-09-03 00:25:46 +0530

    Min & max vel & acc const. accd to cycloidal
    - Add constraints for each interval
    - Store opti data for analysis

--- src/exciting_trajectory/nonlinear_constraints.m
+++ src/exciting_trajectory/nonlinear_constraints.m
@@ -1,42 +1,67 @@
-function [c, ceq] = nonlinear_constraints(tr_par, th_l, th_u, dth_l, dth_u, ddth_l, ddth_u)
-    global fileID iter A_eq b_eq cond_num_reg_mat inverse_signal_strength cost_value;
+function [c, ceq] = nonlinear_constraints(tr_par, min_vel, max_vel, min_acc, max_acc)
+    global fileID iter cond_num_reg_mat inverse_signal_strength cost_value mtum_conserved A_ineq b_ineq;
     num_links = inputs();
     num_joints = num_links - 1;
-    [~, ti, tf, incr, ~, ~] = initials();
-    all_instants = ti : incr : tf;
-    num_instants = length(all_instants);
-    th = zeros(num_joints, num_instants); 
-    dth = zeros(num_joints, num_instants); 
-    ddth = zeros(num_joints, num_instants);
-    for curr_instant_index = 1 : num_instants 
-        [th(:, curr_instant_index), dth(:, curr_instant_index), ddth(:, curr_instant_index)] = ...
-            trajectory(all_instants(curr_instant_index), num_links, tf, tr_par);
-    end
-
-    % Max & min pos, vel and acc with given fourier parameters
-    th_comp_min = min(th,[],2);
-    th_comp_max = max(th,[],2);
-    thd_comp_min = min(dth,[],2);
-    thd_comp_max = max(dth,[],2);
-    thdd_comp_min = min(ddth,[],2);
-    thdd_comp_max = max(ddth,[],2);
-
+    [~, ~, tf, ~, ~, ~] = initials();
+    
+    % Compute min and max joint velocity
+    dth_comp_min = [];
+    dth_comp_max = [];
+    ddth_comp_min = [];
+    ddth_comp_max = [];
+    min_interval_vel = [];
+    max_interval_vel = [];
+    min_interval_acc = [];
+    max_interval_acc = [];
+    time_params = [];
+        
+    for curr_joint = 1 : num_joints
+        num_intervals_each_joint = 2 ^ (num_joints - 1);
+        num_curr_joint_coeff_params = num_intervals_each_joint + 1;
+        num_terms_till_curr_joint = num_curr_joint_coeff_params * (curr_joint - 1);
+        for curr_interval = 1 : num_intervals_each_joint
+            thi = tr_par(num_terms_till_curr_joint + curr_interval);
+            thf = tr_par(num_terms_till_curr_joint + curr_interval + 1);
+            Tp = tf / num_intervals_each_joint;
+            time_params = [time_params; Tp];
+            if thi < thf
+                curr_interval_min_vel = 0;
+                curr_interval_max_vel = (2/Tp) * (thf - thi);
+                curr_interval_min_acc = (2 * pi/Tp^2) * (thi - thf);
+                curr_interval_max_acc = (2 * pi/Tp^2) * (thf - thi);
+            else
+                curr_interval_min_vel = (2/Tp) * (thf - thi);
+                curr_interval_max_vel = 0;
+                curr_interval_min_acc = (2 * pi/Tp^2) * (thf - thi);
+                curr_interval_max_acc = (2 * pi/Tp^2) * (thi - thf);
+            end
+            dth_comp_min = [dth_comp_min; curr_interval_min_vel];
+            dth_comp_max = [dth_comp_max; curr_interval_max_vel];
+            ddth_comp_min = [ddth_comp_min; curr_interval_min_acc];
+            ddth_comp_max = [ddth_comp_max; curr_interval_max_acc];            
+        end
+        min_interval_vel = [min_interval_vel; repmat(min_vel(curr_joint, 1), num_intervals_each_joint, 1)];
+        max_interval_vel = [max_interval_vel; repmat(max_vel(curr_joint, 1), num_intervals_each_joint, 1)];
+        min_interval_acc = [min_interval_acc; repmat(min_acc(curr_joint, 1), num_intervals_each_joint, 1)];
+        max_interval_acc = [max_interval_acc; repmat(max_acc(curr_joint, 1), num_intervals_each_joint, 1)];    
+    end          
+    
     % Inequality constraints (c <= 0)
-    % Checking if the position, vel and acc traj are within the limits (c <= 0)
-    c = [th_comp_max - th_u;
-         th_l - th_comp_min;
-         thd_comp_max - dth_u
-         dth_l - thd_comp_min
-         thdd_comp_max - ddth_u;
-         ddth_l - thdd_comp_min];
+    % Checking if the vel and acc traj are within the limits (c <= 0)
+    c = [min_interval_vel - dth_comp_min;
+         dth_comp_max - max_interval_vel;
+         min_interval_acc - ddth_comp_min;
+         ddth_comp_max - max_interval_acc];
         
     % Equality constraints (ceq = 0) 
-%     ceq = dth(:, 1);
     ceq = [];
     
+    
     % Store data into textfile for post-opti analysis
+    % Iter index
     fprintf(fileID, 'Iter_index = %d\n', iter);
     
+    % Traj params
     lnt = length(tr_par);
     fprintf(fileID, 'tr_par = [');
     for i = 1 : lnt
@@ -44,23 +69,28 @@ function [c, ceq] = nonlinear_constraints(tr_par, th_l, th_u, dth_l, dth_u, ddth
     end
     fprintf(fileID, ']\n');
     
-    initial_vel = (A_eq * tr_par.' - b_eq).';
-    if (abs(initial_vel) < 0.0001)
-        fprintf(fileID, 'Linear satisfied = Yes\n');  
+    % Mtum conservation verification
+    fprintf(fileID, 'Mtum_conserved = %d\n', mtum_conserved);
+    
+    % Linear inequality
+    c_lin = A_ineq * tr_par.' - b_ineq;
+    if c_lin < 10e-3
+        fprintf(fileID, 'Linear inequality satisfied = Yes\n');  
     else
-        fprintf(fileID, 'Linear satisfied = No\n');  
+        fprintf(fileID, 'Linear inequality satisfied = No\n');  
     end
-    lnt = length(initial_vel);
-    fprintf(fileID, 'v0 = [');
+    lnt = length(c_lin);
+    fprintf(fileID, 'c_lin = [');
     for i = 1 : lnt
-        fprintf(fileID, '%f, ', initial_vel(i));
+        fprintf(fileID, '%f, ', c_lin(i));
     end
     fprintf(fileID, ']\n');
     
-    if c < 0
-        fprintf(fileID, 'Nonlinear satisfied = Yes\n');  
+    % Nonlinear inequality
+    if c < 10e-3
+        fprintf(fileID, 'Nonlinear inequality satisfied = Yes\n');  
     else
-        fprintf(fileID, 'Nonlinear satisfied = No\n');  
+        fprintf(fileID, 'Nonlinear inequality satisfied = No\n');  
     end
     lnt = length(c);
     fprintf(fileID, 'c = [');
@@ -69,7 +99,7 @@ function [c, ceq] = nonlinear_constraints(tr_par, th_l, th_u, dth_l, dth_u, ddth
     end
     fprintf(fileID, ']\n');
     
-    fprintf(fileID, 'Con_no = %f, Signal = %f, Total cost = %f\n', cond_num_reg_mat,...
-        inverse_signal_strength, cost_value);
+    fprintf(fileID, 'Con_no = %f, inverse_signal_strength = %f, total_cost = %f\n', ...
+        cond_num_reg_mat, inverse_signal_strength, cost_value);
     fprintf(fileID, '-------------------------------------------------------------\n');
 end
