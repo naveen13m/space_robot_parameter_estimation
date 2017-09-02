4ecd5029f5eba85411eaa1339b54b99e2a99e916
Naveen <naveen.13m@gmail.com>
2017-09-03 00:25:28 +0530

    Minor changes

--- src/exciting_trajectory/joint_limits.m
+++ src/exciting_trajectory/joint_limits.m
@@ -1,9 +1,9 @@
-function [min_jt_angle, max_jt_angle, min_jt_velocity, max_jt_velocity, ...
+function [min_jt_angle, max_jt_angle, min_jt_vel, max_jt_vel, ...
                                    min_jt_acc, max_jt_acc] = joint_limits()
     min_jt_angle = [-90; -110] * pi/180;
     max_jt_angle = [90; 110] * pi/180;
-    min_jt_velocity = [-25; -25] * pi/180;
-    max_jt_velocity = [25; 25] * pi/180;
+    min_jt_vel = [-25; -25] * pi/180;
+    max_jt_vel = [25; 25] * pi/180;
     min_jt_acc = [-15; -15] * pi/180;
     max_jt_acc = [15; 15] * pi/180;
 end
