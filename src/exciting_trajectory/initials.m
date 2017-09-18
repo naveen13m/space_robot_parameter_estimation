90969d0bf3ad2020ac0beae0cf8ed360df6f629e
Naveen <naveen.13m@gmail.com>
2017-09-19 02:10:17 +0530

    Put 3 mutually orth. RWs on the base

--- src/exciting_trajectory/initials.m
+++ src/exciting_trajectory/initials.m
@@ -1,23 +1,31 @@
-function [y0, ti, tf, incr, rtol, atol, vel_combi_mat] = initials()
-    ti=0; tf=20;
-    n = inputs();
+function [y0, t_initial, t_final, incr, rtol, atol, vel_combi_mat]=initials()
 
-    %Base motions
-    q=[0; 0; 0; 0; 0; 0];
-    dq=[0; 0; 0; 0; 0; 0];
+%Simulation time
+t_initial=0;
+t_final=64;
 
-    % Actuator energy
-    acten=0;
+%Inverse kinematics for obtaining initial configuration
+num_links = inputs();
 
-    %Vector of all the initial State Variable
-    y0=[q; dq; acten];
+%Base motions
+q=[0; 0; 0; 0; 0; 0];
+dq=[0; 0; 0; 0; 0; 0];
 
-    %INTERATION TOLERANCES
-    incr=0.1;
-    rtol=1e-5;         %relative tolerance in integration 
-    atol=1e-7;         %absolute tolerances in integration
-    
-    % Velocity combination matrix for traj optimization
-    vel_combi_mat = [1 1 
-                     1 0];
+% Actuator energy
+acten=0;
+
+%Vecotor of all the initial State Variable
+y0=[q; dq; acten];
+
+%INTERATION TOLERANCES
+incr=0.2;
+rtol=1e-5;         %relative tolerance in integration 
+atol=1e-7;         %absolute tolerances in integration 
+
+num_arm_joints = num_links - 1 - 4;
+vel_combi_mat = generate_vel_combi_mat(num_arm_joints);
+unordered_jt_index = 1 : num_arm_joints;
+ordered_jt_index = [1 3 5 2 4 6];
+vel_combi_mat(:, unordered_jt_index) = vel_combi_mat(:, ordered_jt_index);
+
 end
