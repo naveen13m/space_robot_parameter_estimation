b04f7562a3eca1f56c037c7699df278dcf3674cb
Naveen <naveen.13m@gmail.com>
2017-10-11 01:26:41 +0530

    Minor changes

--- src/boundary_value_least_squares.m
+++ src/boundary_value_least_squares.m
@@ -4,6 +4,6 @@ function [param_vec, res_norm, exit_flag] = boundary_value_least_squares(reg_mat
     problem = createOptimProblem('fmincon', 'objective', ...
         @(x)norm(reg_mat * x.' - out_vec), 'lb', lb, 'ub', ub, 'x0', ub, ...
         'options', opts);
-    [param_vec, res_norm] = run(gs,problem);
+    [param_vec, res_norm, exit_flag] = run(gs,problem);
 end
 
--- src/run_me.m
+++ src/run_me.m
@@ -1,4 +1,4 @@
-    function [reg_mat, out_vec, global_kin_mat] = run_me(robot_make, ...
+    function [reg_mat, out_vec, global_kin_mat, mtvar_reshaped] = run_me(robot_make, ...
                                 base_sensor_base_frame_position_base_frame)
     close all; clc;
     
@@ -34,58 +34,76 @@
                                 num_links, not_planar, joint_twist, link_length_DH, ...
                                 joint_offset, parent_link_index, base_sensor_base_frame_position_base_frame);
    
-
+%     % Parameter coupling matrix verification
+%     load param_coupling_mat.mat
+%     error_minimal_coup_mat = param_coupling_mat * param_vec - actual_minimal_param_vec
+                            
     % Load robot structural data
-%     cd(strcat('../test_case_data', robot_make, '/config_files'));
-%     [num_links, not_planar] = inputs();
+    cd(strcat('../test_case_data', robot_make, '/config_files'));
+    [num_links, not_planar] = inputs();
     is_planar = 1 - not_planar;
-%     cd(curr_dir);
+    cd(curr_dir);
     rw_params = make_reaction_wheel();
     
     global_kin_mat = global_kinematic_matrix(robot_make, ...
-        base_sensor_base_frame_position_base_frame, statevar, mtvar);
+        base_sensor_base_frame_position_base_frame, statevar, mtvar, 0);
     [reg_mat, out_vec] = reduce_gkm(global_kin_mat, is_planar, rw_params);
-
-
-    minimal_param_vec = pinv(reg_mat) * (out_vec);
-
-%     num_intervals = 64;
-%     num_datapoints_per_interval = 10 - 0;
-%     num_equs = 6;
-%     num_rows = num_datapoints_per_interval * num_equs; 
-%     
-%     cn = zeros(num_intervals, 1);
-%     for curr_interval = 1 : num_intervals
-%         cn(curr_interval) = cond(reg_mat(1 : curr_interval * num_rows, :));
-%     end
+    mtvar_reshaped = reshape(mtvar.', numel(mtvar), 1);
+    out_vec = out_vec + randn(size(out_vec));
+    minimal_param_vec_unfiltered = pinv(reg_mat) * (out_vec);
     
-%     figure();
-%     subplot(3, 1, 1)
-%     semilogy(cn); grid on;
-%     subplot(3, 1, 2)
-%     plot(cn(1 : 8)); grid on;
-%     subplot(3, 1, 3)
-%     semilogy(cn(9 : end)); grid on;
+    check_int = 208;
+    minimal_param_vec_other_sol = pinv(reg_mat(1 : (check_int * 5 * 6), :)) * (out_vec(1 : (check_int * 5 * 6)));
+
+    num_intervals = 256;
+    num_datapoints_per_interval = 5 - 0;
+    num_equs = 6;
+    num_rows = num_datapoints_per_interval * num_equs; 
     
+    cn = zeros(num_intervals, 1);
+    for curr_interval = 1 : num_intervals
+        cn(curr_interval) = cond(reg_mat(1 : curr_interval * num_rows, :));
+    end
+    
+    figure();
+    subplot(2, 1, 1)
+    semilogy(cn, 'k'); 
+    ylabel('Condition Number', 'FontSize', 13);
+    xlabel('Interval Index', 'FontSize', 13);
+    set(gca, 'FontSize', 12,  'FontName', 'times')
+    subplot(2, 1, 2)
+    semilogy(129 : 256, cn(129 : end), 'k'); 
+    ylabel('Condition Number', 'FontSize', 13);
+    xlabel('Interval Index', 'FontSize', 13);
+    set(gca, 'FontSize', 12, 'FontName', 'times')
+%     print('cond_num', '-deps', '-r300');
                             
-    compare = [actual_minimal_param_vec, minimal_param_vec]
-    param_error = minimal_param_vec - actual_minimal_param_vec;
-    per_error_min_parm = (param_error./ actual_minimal_param_vec) * 100
-    mtum = reg_mat * minimal_param_vec - out_vec;
+    compare = [(1 : 52).', actual_minimal_param_vec, minimal_param_vec_unfiltered, minimal_param_vec_other_sol]
+    save actual_computed_params.mat actual_minimal_param_vec minimal_param_vec_unfiltered param_vec
+    param_error_unfiltered = minimal_param_vec_unfiltered - actual_minimal_param_vec;
+    per_error_min_parm = [(1 : 52).', (param_error_unfiltered./ actual_minimal_param_vec) * 100];
+    mean_error = (1/52) * sum(abs(minimal_param_vec_unfiltered ./ actual_minimal_param_vec));
+    mtum = reg_mat * minimal_param_vec_unfiltered - out_vec;
     mtum1 = reg_mat * actual_minimal_param_vec - out_vec;
     
     figure;
     subplot(2, 3, 1);
-    plot(mtum(1 : 6 : end)); hold on; plot(mtum1(1 : 6 : end)); grid on; title('Px_{RW}');
+    plot(mtum(1 : 6 : end), 'k'); hold on; 
+    plot(mtum1(1 : 6 : end), 'g--'); grid on;
     subplot(2, 3, 2);
-    plot(mtum(2 : 6 : end)); hold on; plot(mtum1(2 : 6 : end)); grid on; title('Py_{RW}');
+    plot(mtum(2 : 6 : end), 'k'); hold on; 
+    plot(mtum1(2 : 6 : end), 'g--'); grid on;
     subplot(2, 3, 3);
-    plot(mtum(3 : 6 : end)); hold on; plot(mtum1(3 : 6 : end)); grid on; title('Pz_{RW}');
+    plot(mtum(3 : 6 : end), 'k'); hold on; 
+    plot(mtum1(3 : 6 : end), 'g--'); grid on;
     subplot(2, 3, 4);
-    plot(mtum(4 : 6 : end)); hold on; plot(mtum1(4 : 6 : end)); grid on; title('Lx_{RW}');
+    plot(mtum(4 : 6 : end), 'k'); hold on; 
+    plot(mtum1(4 : 6 : end), 'g--'); grid on;
     subplot(2, 3, 5);
-    plot(mtum(5 : 6 : end)); hold on; plot(mtum1(5 : 6 : end)); grid on; title('Ly_{RW}');
+    plot(mtum(5 : 6 : end), 'k'); hold on; 
+    plot(mtum1(5 : 6 : end), 'g--'); grid on;
     subplot(2, 3, 6);
-    plot(mtum(6 : 6 : end)); hold on; plot(mtum1(6 : 6 : end)); grid on; title('Lz_{RW}');
-% %     close all
+    plot(mtum(6 : 6 : end), 'k'); hold on; 
+    plot(mtum1(6 : 6 : end), 'g--'); grid on;
+%     close all
 end
