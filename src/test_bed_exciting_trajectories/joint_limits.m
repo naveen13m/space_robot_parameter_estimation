078d4a4f942c68bdb0f85eaffa6d7f942a4b8a33
Naveen <naveen.13m@gmail.com>
2017-11-14 11:53:36 +0530

    increased joint limits

--- src/test_bed_exciting_trajectories/joint_limits.m
+++ src/test_bed_exciting_trajectories/joint_limits.m
@@ -1,12 +1,12 @@
 function [min_jt_angle, max_jt_angle, min_jt_vel, max_jt_vel, ...
                                    min_jt_acc, max_jt_acc] = joint_limits()
-    min_jt_angle = [-90; -45; 90; -45] * pi/180;
-    max_jt_angle = [90; 45; 270; 45] * pi/180;
+    min_jt_angle = [-95; -50; 85; -50] * pi/180;
+    max_jt_angle = [95; 50; 275; 50] * pi/180;
 %     min_jt_angle = [-90; -90; 90; -90] * pi/180;
 %     max_jt_angle = [90; 90; 270; 90] * pi/180;
-    min_jt_vel = [-60; -60; -60; -60] * pi/180;
-    max_jt_vel = [60; 60; 60; 60] * pi/180;
-    min_jt_acc = [-360; -360; -360; -360] * pi/180;
-    max_jt_acc = [360; 360; 360; 360] * pi/180;
+    min_jt_vel = [-300; -300; -300; -300] * pi/180;
+    max_jt_vel = [300; 300; 300; 300] * pi/180;
+    min_jt_acc = [-1000; -1000; -1000; -1000] * pi/180;
+    max_jt_acc = [1000; 1000; 1000; 1000] * pi/180;
 end
 
