b1f53d3f2bf36e0ec0d0560aca00632133552b10
Naveen <naveen.13m@gmail.com>
2017-11-14 11:53:12 +0530

    Trajectories with broader limits

--- src/test_bed_exciting_trajectories/debugger.m
+++ src/test_bed_exciting_trajectories/debugger.m
@@ -4,13 +4,16 @@ clear all; close all; clc
 % dbstop in trajectory.m at 8
 global joint_pos joint_vel joint_acc
 joint_pos = []; joint_vel = []; joint_acc = [];
-tr_par_seed = [-1.22313890076491,0.303451868445267,3.20179188236482,0.108153706584789,0.153910196489147,0.0440378283370745,0.135180645621238,0.0440693325406989];
+tr_par_seed = [-1.57079632679490,-0.785398163397448,3.60843332079036,0.762242186278960,...
+                  0.195837599867740,0.121194647269874,0.142212304301809,-0.188321399987424];
+% tr_par_seed = [-1.35960511730358,-0.812196950854437,1.95696450220549,0.0622121638219488,...
+%                 0.212127604938039,0.443741851888763,0.440421994324858,-0.414733845197917];
 num_arm_joints = 4;
 num_rw_joints = 1;
 rw_seed_params = [0, 2*pi/3];
 tr_par_seed = [tr_par_seed(1 : num_arm_joints), rw_seed_params(1), tr_par_seed(num_arm_joints + 1 : end), rw_seed_params(2)];
 num_joints = num_arm_joints + num_rw_joints;
-t_diff = 0.05;
+t_diff = 0.1;
 % load pruned_data.mat
 % tf=numel(pruned_interval_index);
 tf=16;
@@ -46,20 +49,20 @@ for curr_joint = 1 : num_joints
         subplot(3, 1, plot_index);
         if plot_index == 1
             kin_data = position;
-%             ll = (180 / pi) * min_jt_angle(curr_joint) * ones(1,  num_instants);
-%             ul = (180 / pi) * max_jt_angle(curr_joint) * ones(1,  num_instants);
+            ll = (180 / pi) * min_jt_angle(curr_joint) * ones(1,  num_instants);
+            ul = (180 / pi) * max_jt_angle(curr_joint) * ones(1,  num_instants);
         elseif plot_index == 2
             kin_data = velocity;
-%             ll = (180 / pi) * min_jt_velocity(curr_joint) * ones(1,  num_instants);
-%             ul = (180 / pi) * max_jt_velocity(curr_joint) * ones(1,  num_instants);
+            ll = (180 / pi) * min_jt_velocity(curr_joint) * ones(1,  num_instants);
+            ul = (180 / pi) * max_jt_velocity(curr_joint) * ones(1,  num_instants);
         else
             kin_data = acceleration;
-%             ll = (180 / pi) * min_jt_acc(curr_joint) * ones(1,  num_instants);
-%             ul = (180 / pi) * max_jt_acc(curr_joint) * ones(1,  num_instants);
+            ll = (180 / pi) * min_jt_acc(curr_joint) * ones(1,  num_instants);
+            ul = (180 / pi) * max_jt_acc(curr_joint) * ones(1,  num_instants);
         end
         plot(total_time, (180 / pi) * kin_data(curr_joint, :)); hold on;
-%         plot(total_time, ll, 'r');
-%         plot(total_time, ul, 'r');
+        plot(total_time, ll, 'r');
+        plot(total_time, ul, 'r');
         title(strcat('Joint-', num2str(curr_joint), kin_data_name{plot_index}));
         grid on;
     end
